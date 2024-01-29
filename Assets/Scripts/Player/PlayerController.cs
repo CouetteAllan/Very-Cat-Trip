559834188d7fcff28e236923ea33fad386640f60
COUETTE Allan <55735176+CouetteAllan@users.noreply.github.com>
2024-01-29 17:04:58 +0100

    Debut texture sol + character done

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rayqdr.CatInputs;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private MInputsAction _inputs;
+    public MInputsAction Inputs {
+        get 
+        {
+            if( _inputs == null)
+            {
+                _inputs = new MInputsAction();
+                _inputs.Enable();
+            }
+
+            return _inputs;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        if(_inputs == null)
+        {
+            _inputs = new MInputsAction();
+            _inputs.Enable();
+        }
+
+
+            
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,27 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Rayqdr.CatInputs;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float _horizontalSpeed = 40.0f;
+    [SerializeField] private float _jumpForce = 20.0f;
+
+
+    [Space]
+    [SerializeField] private Transform _groundOffsetPoint;
+    [SerializeField] private Vector2 _groundBoxCastSize;
+    [SerializeField] private float _castDistance;
+    [SerializeField] private LayerMask _groundMask;
+    [SerializeField] private Transform _centerOfMassOffset;
+
     private Rigidbody2D _rb;
+    private MInputsAction _inputAction;
+    private PlayerController _playerController;
+
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _rb.centerOfMass = _centerOfMassOffset.localPosition;
+
+        _playerController = GetComponent<PlayerController>();
     }
 
     void Start()
     {
-        _rb.velocity = Vector2.right * 5.0f;
+        _inputAction = _playerController.Inputs;
+        _inputAction.Player.Jump.performed += Jump_performed;
+
+        _inputAction.Enable();
     }
 
+    private void Jump_performed(InputAction.CallbackContext obj)
+    {
+        if (IsGrounded())
+        {
+            _rb.AddForce(Vector2.up * _jumpForce,ForceMode2D.Impulse);
+            //play anim
+        }
+    }
 
     private void FixedUpdate()
     {
-        _rb.AddForce(Vector2.right * 25.0f);
-        var clampedVelocityX = Mathf.Clamp(_rb.velocity.x, -25.0f, 25.0f);
+        _rb.AddForce(transform.right * _horizontalSpeed);
+        var clampedVelocityX = Mathf.Clamp(_rb.velocity.x, -25.0f, 30.0f);
         var newClampedVelocity = new Vector2(clampedVelocityX,_rb.velocity.y);
         _rb.velocity = newClampedVelocity;
+    }
+
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(_groundOffsetPoint.position, _groundBoxCastSize, 0, -transform.up, _castDistance, _groundMask);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(_groundOffsetPoint.position -transform.up * _castDistance, _groundBoxCastSize);
+    }
+
+    private void OnDisable()
+    {
+        _inputAction.Player.Jump.performed -= Jump_performed;
+
     }
 }
